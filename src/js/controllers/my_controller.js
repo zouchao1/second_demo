65b977218351bb22b7e05eacf4e709717819f9cb
2541736340@qq.com <2541736340@qq.com>
2018-10-21 23:11:35 +0800

    'my'

--- src/js/controllers/my_controller.js
+++ src/js/controllers/my_controller.js
@@ -4,6 +4,10 @@ const render = () => {
     $(".nav-link:not(:first-child)").on('tap',function(){
         $(".header").css("display",'none')
     })
+    $(".s-con>.up-in>a").on('tap',()=>{
+       // console.log("aaaa");
+        //window.location.href='https://h5.ele.me/login/#redirect=https%3A%2F%2Fh5.ele.me%2Fprofile%2F';
+    })
     $(' main').html(my_template);
 }
 
